7732b76124b8c025c68ee2e1c53ae7fa8b08da99
rlagnltjd <yhooi2003@naver.com>
2023-04-17 13:51:40 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="posts/first-post">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-    return <h1>I Love ZeonSanHak</h1>;
-  }
+  return (
+    <div>
+      <h1>I Love ZeonSanHak</h1>
+      <Link href="/">Back to home</Link>
+      <div>
+        <button>과제 완료</button>
+      </div>
+      <div>
+        <a href="/script.html">제발 나좀 살려줘</a>
+      </div>
+    </div>
+  );
+}
